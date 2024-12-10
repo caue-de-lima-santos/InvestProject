58f8b379c680fec689da3112556a48313db85251
caue-de-lima-santos <cauestud9@gmail.com>
2024-12-09 22:02:19 -0300

    second att, css deletado e melhorias de back-end

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-const jurosSimples = ()=>{
+const rendimento = ()=>{
     init = Number(document.getElementById("capitalinicial").value)
     taxa = Number(document.getElementById("taxa").value)
     time = Number(document.getElementById("tempo").value)
-    resultado = (init*taxa*time)
-    document.getElementById("resultado").textContent=`O juros simples é ${resultado}`
+    resultado2 = ((init*((1+taxa)**time))-init).toFixed(2)
+    document.getElementById("resultado").textContent=`O rendimento nessas condições é ${resultado2}`
+    const meses = []
+    for (let i=0; i<time; i++){
+        const mont = (init*((1+taxa)**i))
+        meses.push(mont)
+    }
+    meses1 = meses.map(x=>{
+        const posicao = (Number(meses.indexOf(x))+1) + "°"
+        return `O montante no ${posicao} mês é ${(x).toFixed(2)}`
+    })
+    document.getElementById("evolu").textContent=`Crescimento mensal do montante: ${meses1}`
 }
-const MontanteSimples = ()=>{
-    init = Number(document.getElementById("capitalinicial").value)
-    taxa = Number(document.getElementById("taxa").value)
-    time = Number(document.getElementById("tempo").value)
-    resultado = init+(taxa*time*init)
-    document.getElementById("resultado").textContent=`O montante simples é ${resultado}`
-}
-const MontanteCompostos = ()=>{
-    init = Number(document.getElementById("capitalinicial").value)
-    taxa = Number(document.getElementById("taxa").value)
-    time = Number(document.getElementById("tempo").value)
-    resultado = (init*((1+taxa)**time)).toFixed(2)
-    document.getElementById("resultado").textContent=`O montante composto é ${resultado}`
-}
-const jurosCompostos = ()=>{
-    init = Number(document.getElementById("capitalinicial").value)
-    taxa = Number(document.getElementById("taxa").value)
-    time = Number(document.getElementById("tempo").value)
-    resultado = ((init*((1+taxa)**time))-init).toFixed(2)
-    document.getElementById("resultado").textContent=`O juros composto é ${resultado}`
-}
-console.log(MontanteCompostos(250)(0.01, 1200))
-console.log(MontanteSimples(250)(0.01, 1200))
